ad6744cba40644dc89af1d20724f792690b018d5
ksernag <kserna.g@gmail.com>
2023-05-24 19:51:16 -0500

    new files structure

---
+++ java_scr_ejercicios/curso_03_java_ejercicio_1/src/com/example/ejericicio1_tipos_datos.java
@@ -0,0 +1,44 @@
+package com.example;
+
+public class ejericicio1_tipos_datos {
+    public static void main(String[]args) {
+
+        // numeros enteros
+
+        byte numero1 = 8;
+        short numero2 = 15;
+        int numero3 = 40;
+        long numero4 = 500;
+
+        System.out.println("byte = " + numero1);
+        System.out.println("short = " + numero2);
+        System.out.println("int = " + numero3);
+        System.out.println("long = " + numero4);
+
+        // numeros decimales
+
+        float decimal1 = 5.6f;
+        double decimal2 = 239.54d;
+
+        System.out.println("float = " + decimal1);
+        System.out.println("double = " + decimal2);
+
+        // booleanos
+
+        boolean variable1 = true;
+        boolean variable2 = false;
+
+        System.out.println("boolean = " + variable1);
+        System.out.println("boolean = " + variable2);
+
+        // texto
+
+        char variable3 = 'g';
+        String variable4 = "keep calm";
+
+        System.out.println("char = " + variable3);
+        System.out.println("String = " + variable4);
+
+
+    }
+}
